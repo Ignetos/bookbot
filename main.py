8174a08107536c5f89206006065b0f4a4eb91379
Ignetos <krisnain.dev@gmail.com>
2025-01-15 01:00:05 +0700

    added sorting and reporting function

--- main.py
+++ main.py
@@ -3,8 +3,22 @@ def main():
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     character_count = count_num_char(text)
+    sorted_dict = dict(sort_list(character_count))
+
+    print("--- Begin report of books/frankenstein.txt ---")
     print(f"{num_words} words found in the document")
-    print(character_count)
+    print("")
+
+    for i in sorted_dict:
+        print(f"The {i} character was found {sorted_dict[i]} times")
+
+    print("--- End report ---")
+
+
+def sort_list(dict):
+    sorted_dict = sorted(dict.items(), key=lambda x:x[1], reverse=True)
+    return sorted_dict
+
 
 def count_num_char(text):
     lowered_text = text.lower()
@@ -12,8 +26,9 @@ def count_num_char(text):
     for char in lowered_text:
         if char in char_count:
             char_count[char] += 1
-        else:
+        elif char.isalpha():
             char_count[char] = 1
+
     return char_count
 
 def get_num_words(text):
