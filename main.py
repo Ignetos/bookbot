ce6fc3af0332d62f2ca484271155287906cf7b9e
Ignetos <krisnain.dev@gmail.com>
2025-01-14 18:04:49 +0700

    added count_num_char() function

--- main.py
+++ main.py
@@ -2,7 +2,19 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
+    character_count = count_num_char(text)
     print(f"{num_words} words found in the document")
+    print(character_count)
+
+def count_num_char(text):
+    lowered_text = text.lower()
+    char_count = {}
+    for char in lowered_text:
+        if char in char_count:
+            char_count[char] += 1
+        else:
+            char_count[char] = 1
+    return char_count
 
 def get_num_words(text):
     words = text.split()
